4e9826ade8a806de34a68e39038f48b56d75e4f6
Jang355 <iiss1010@naver.com>
2021-01-08 16:45:22 +0000

    :yellow_heart: JANG :yellow_heart: (21/01/09)

--- Programmers/Level 2/튜플/Solution.cpp
+++ Programmers/Level 2/튜플/Solution.cpp
@@ -4,41 +4,43 @@
 
 using namespace std;
 
-int isin(string s, vector<pair<string,int>> vec){
+int isin(int s, vector<pair<int,int>> vec){
     for(int i=0;i<vec.size();i++)
         if(vec[i].first==s)
             return i;
     return -1;
 }
 
-bool compare(pair<string,int> a, pair<string,int> b) {
+bool compare(pair<int,int> a, pair<int,int> b) {
     if(a.second == b.second) return a.first<b.first;
     return a.second>b.second;
 }
 
 vector<int> solution(string s) {
     vector<int> answer;
-    vector<pair<string,int>> tuple;
-    string tmp = "";
-    
+    vector<pair<int,int>> tuple;
+    int tmp = 0;
+
     for(int i=0;i<s.size();i++){
-        if(s[i]>=48&&s[i]<=57)
-            tmp += s[i];
-        else if((s[i]==','||s[i]=='}')&&tmp!=""){
+        if(s[i]>=48&&s[i]<=57){
+            tmp *= 10;
+            tmp += s[i]-'0';
+        }
+        else if((s[i]==','||s[i]=='}')&&tmp!=0){
             if(isin(tmp,tuple)==-1)
                 tuple.push_back(make_pair(tmp,1));
             else     
                 tuple[isin(tmp,tuple)].second++;
-            tmp = "";
+            tmp = 0;
         }
     }
-    
+
     sort(tuple.begin(),tuple.end(),compare);
-    
+
     for(int i=0;i<tuple.size();i++){
-        int n = atoi(tuple[i].first.c_str());
+        int n = tuple[i].first;
         answer.push_back(n);
     }
-    
+
     return answer;
 }
